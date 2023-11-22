83a5f269401e0552152dc81975304a7b0a2d7790
Ting-Yu Chen <78402930+7ingyu@users.noreply.github.com>
2023-11-22 15:15:57 -0800

    Time: 67 ms (44.22%), Space: 44.6 MB (48.68%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -3,14 +3,6 @@
  * @return {number}
  */
 var removeDuplicates = function(nums) {
-    let current = nums[0]
-    for (let i = 1; i < nums.length; i++) {
-        if (nums[i] === current) {
-            nums.splice(i, 1)
-            i --
-        } else {
-            current = nums[i]
-        }
-    }
+    nums.splice(0, nums.length, ... new Set(nums))
     return nums.length
 };
