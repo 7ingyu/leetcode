759f5d59ada40ad868482d45445411012cba8ed2
Ting-Yu Chen <78402930+7ingyu@users.noreply.github.com>
2024-03-25 20:48:50 -0700

    Time: 59 ms (66.23%), Space: 17.7 MB (41.50%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,19 +1,11 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        lst = list(enumerate(nums))
-        # print(f"enumerated: {lst}")
-        lst = sorted(lst, key=lambda num: num[1])
-        # print(f"sorted: {lst}")
-        left = 0
-        right = len(lst) - 1
-        while left < right:
-            l_idx, l_val = lst[left]
-            r_idx, r_val = lst[right]
-            if l_val + r_val == target:
-                return l_idx, r_idx
-            elif l_val + r_val > target:
-                right -= 1
-            elif l_val + r_val < target:
-                left += 1
+        record = {}  
+        
+        for i, num in enumerate(nums):
+            diff = target - num
+            if diff in record:
+                return [record[diff], i] 
+            record[num] = i
         
         
